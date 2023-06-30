d218a3f01b87b2c42679b52d50a76818e2bca237
Bryam Anderson Villalobos De Leon <bryamv@uninorte.edu.co>
2023-06-29 23:03:31 -0500

    cards created

---
+++ src/components/heroes/HeroCard.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+export const HeroCard = ({
+  id,
+  superhero,
+  publisher,
+  alter_ego,
+  first_appearance,
+  characters,
+}) => {
+  return (
+    <>
+      <div className="card m-2" style={{ width: "18rem" }}>
+        <img
+          src={`./assets/heroes/${id}.jpg`}
+          className="card-img-top"
+          alt={superhero}
+        />
+        <div className="card-body">
+          <h5 className="card-title">{superhero}</h5>
+          <p className="card-text">{alter_ego}</p>
+          {alter_ego !== characters && (
+            <p className="card-text">{characters} </p>
+          )}
+          <p className="card-text">
+            <small className="text-muted">{first_appearance}</small>
+          </p>
+          <Link to={`./hero/${id}`} className="btn btn-primary">
+            Ver más
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/components/heroes/HeroList.jsx
+++ src/components/heroes/HeroList.jsx
@@ -1,12 +1,13 @@
 import { getHeroesByPublisher } from "../../selectors/getHeroesByPublisher";
+import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher);
   return (
-    <ul>
+    <div className="row row-cols-1 row-cols-md-3 g-4">
       {heroes.map((hero) => (
-        <li key={hero.id}>{hero.superhero}</li>
+        <HeroCard key={hero.id} {...hero} />
       ))}
-    </ul>
+    </div>
   );
 };
